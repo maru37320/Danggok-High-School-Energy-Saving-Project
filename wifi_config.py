3ae91e20d87ccdb422c9a20837c1da273b7fc912
maru37320 <maru37320@gmail.com>
2026-06-10 02:29:05 +0900

    Create wifi_config.py

---
+++ wifi_config.py
@@ -0,0 +1,7 @@
+WIFI_NETWORKS = {
+    "senWiFi_Free": "dghs!@2021"
+}
+
+SHEET_URL = "https://script.google.com/macros/s/AKfycbwssGqCtO0_EDCUJRwkydoIk_ut_uHFfomZwEpu7dxOBftMUGZgg7r_AwaMavP7DtNLCw/exec"
+
+CLASS_ID = "!!여기 부분에 입력!!"
