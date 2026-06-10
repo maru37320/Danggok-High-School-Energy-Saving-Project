540ccebea4b1a032ff705cde27b1ce8223185e1b
maru37320 <maru37320@gmail.com>
2026-06-10 22:49:45 +0900

    Update wifi_config.py

--- wifi_config.py
+++ wifi_config.py
@@ -4,4 +4,4 @@ WIFI_NETWORKS = {
 
 SHEET_URL = "https://script.google.com/macros/s/AKfycbwssGqCtO0_EDCUJRwkydoIk_ut_uHFfomZwEpu7dxOBftMUGZgg7r_AwaMavP7DtNLCw/exec"
 
-CLASS_ID = "!!여기 부분에 입력!!"
+CLASS_ID = "!!!여기 부분에 입력!!!"
